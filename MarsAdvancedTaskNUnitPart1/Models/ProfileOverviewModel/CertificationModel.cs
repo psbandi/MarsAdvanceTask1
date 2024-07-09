e75ec213b420c74fb3c1430f0efeba1658803060
psbandi <psbandi984@gmail.com>
2024-07-10 01:17:03 +1200

    initial commit

---
+++ MarsAdvancedTaskNUnitPart1/Models/ProfileOverviewModel/CertificationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsAdvancedTaskNUnitPart1.Models.ProfileOverviewModel
+{
+    public class CertificationModel
+    {
+        public string OriginalCertificate { get; set; }
+        public string Certificate { get; set; }
+        public string From { get; set; }
+        public string Year { get; set; }
+        public string AssertionMessage { get; set; }
+        public bool IsValid { get; set; }
+
+    }
+}
